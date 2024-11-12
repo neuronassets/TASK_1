bac744f46a61a3f9374a7d246c23816fef6bc3bb
neuronassets <ferdicolli94@gmail.com>
2024-11-12 14:26:59 +0100

    Update main.php
    - utilizzo nuovo metodo arrotondaOrario()

--- main.php
+++ main.php
@@ -1,26 +1,25 @@
 <?php
 
-include 'arrotonda_entrata.php';
-include 'arrotonda_uscita.php';
+include 'arrotonda_orario.php';
 
-// example for entry 
+// example for entry
 
 $datetimeEntry = new DateTime("2024-11-09 8:05");
-$roundedEntrata = arrotondaEntrata($datetimeEntry, 15, 5);
+$roundedEntrata = arrotondaOrario($datetimeEntry, 15, 5, TipoArrotondamento::INGRESSO);
 echo "Entrata: " . $roundedEntrata->format("Y-m-d H:i") . "\n";
 
 $datetimeEntry = new DateTime("2024-11-09 08:06");
-$roundedEntrata = arrotondaEntrata($datetimeEntry, 15, 5);
+$roundedEntrata = arrotondaOrario($datetimeEntry, 15, 5, TipoArrotondamento::INGRESSO);
 echo "Entrata: " . $roundedEntrata->format("Y-m-d H:i") . "\n";
 
-// example for exit 
+// example for exit
 
 $datetimeExit = new DateTime("2024-11-09 17:54");
-$roundedUscita = arrotondaUscita($datetimeExit, 15, 5);
+$roundedUscita = arrotondaOrario($datetimeExit, 15, 5, TipoArrotondamento::USCITA);
 echo "Uscita: " . $roundedUscita->format("Y-m-d H:i") . "\n";
 
 $datetimeExit = new DateTime("2024-11-09 17:55");
-$roundedUscita = arrotondaUscita($datetimeExit, 15, 5);
+$roundedUscita = arrotondaOrario($datetimeExit, 15, 5, TipoArrotondamento::USCITA);
 echo "Uscita: " . $roundedUscita->format("Y-m-d H:i") . "\n";
 
 ?>
